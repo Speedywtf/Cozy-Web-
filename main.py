fcbd2ae0431de1d1c21f71ac5f9cb9a3a1c5de4f
Speedywtf <67850051+Speedywtf@users.noreply.github.com>
2022-01-18 20:19:43 +0530

    finallyopenedjsonfile

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+import json 
 
+
+with open('users.test.json', "rb") as file: 
+    db = json.load(file)
+    items = []
+    for item in db:
+        items.append(item['username'])
+    print(items)
 
 
 
