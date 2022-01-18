943a7414aa9f1a419cebf045d84f39db50c4b56a
Speedywtf <67850051+Speedywtf@users.noreply.github.com>
2022-01-18 20:52:22 +0530

    DoneABit

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import json
 
 with open('users.test.json', "rb") as file: 
     db = json.load(file)
-    items = []
-    for item in db:
-        items.append(item['username'])
-    print(items)
+    for elements in db: 
+        if 'system' in elements and 'Locale' in elements and 'flags' in elements and 'discriminator' in elements and 'avatar' in elements and 'LastMessageChannelId' in elements: 
+            del elements['system']
+    
+    
 
 
 
