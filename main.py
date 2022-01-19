5ba9c2348ea0a7ae2bebc2ea419c24f6a1b0705a
Speedywtf <67850051+Speedywtf@users.noreply.github.com>
2022-01-19 09:03:11 +0530

    RemoveCodeFoundSuccessfully

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ import json
 
 with open('users.test.json', "rb") as file: 
     db = json.load(file)
-    for elements in db: 
-        if 'system' in elements and 'Locale' in elements and 'flags' in elements and 'discriminator' in elements and 'avatar' in elements and 'LastMessageChannelId' in elements: 
-            del elements['system']
+    for i in range(len(db)): 
+        if type(db[i]["username"]) == str: 
+            del[db[i]["username"]]
+            
+            
+            
+open("updated.text.json", "w").write(
+    json.dumps(db, sort_keys=True, indent=4, separators=(',', ': '))
+)
     
     
 
