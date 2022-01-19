11f77d026c6cf010e6531212c3b81afa8f5bfaa2
Speedywtf <67850051+Speedywtf@users.noreply.github.com>
2022-01-19 09:23:44 +0530

    CodeFInished

--- main.py
+++ main.py
@@ -4,8 +4,18 @@ import json
 with open('users.test.json', "rb") as file: 
     db = json.load(file)
     for i in range(len(db)): 
-        if type(db[i]["username"]) == str: 
-            del[db[i]["username"]]
+        if db[i]["flags"] == None: 
+            del[db[i]["flags"]]
+        if db[i]["system"] == None: 
+            del[db[i]["system"]]
+        if type(db[i]["createdTimestamp"]) == int: 
+            del[db[i]["createdTimestamp"]]
+        if db[i]["avatar"] == None: 
+            del[db[i]["avatar"]]
+        if db[i]["lastMessageChannelID"] == None: 
+            del[db[i]["lastMessageChannelID"]]
+        if type(db[i]["discriminator"]) == str: 
+            del[db[i]["discriminator"]]
             
             
             
