917e61805d42f68126d71f5fb250e4bcf7674f60
Speedywtf <67850051+Speedywtf@users.noreply.github.com>
2022-01-19 10:19:28 +0530

    DOne

--- main.py
+++ main.py
@@ -8,34 +8,7 @@ with open('users.test.json', "rb") as file:
         items[db[i]["id"]] = db[i]
     
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        """if db[i]["flags"] == None: 
-            del[db[i]["flags"]]
-        if db[i]["system"] == None: 
-            del[db[i]["system"]]
-        if type(db[i]["createdTimestamp"]) == int: 
-            del[db[i]["createdTimestamp"]]
-        if db[i]["avatar"] == None: 
-            del[db[i]["avatar"]]
-        if db[i]["lastMessageChannelID"] == None: 
-            del[db[i]["lastMessageChannelID"]]
-        if type(db[i]["discriminator"]) == str: 
-            del[db[i]["discriminator"]]"""
+    
             
        
             
